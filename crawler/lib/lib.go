e70f1a2347947fa7d1d547a1f485fa260291df18
Pablo Trinidad <p@pablotrinidad.me>
2021-02-18 20:31:27 -0600

    Completed parsing of pages

--- crawler/lib/lib.go
+++ crawler/lib/lib.go
@@ -60,6 +60,71 @@ type course struct {
 	Name string `json:"asignatura__nombre"`
 }
 
+type courseOfferPayload struct {
+	Container *scheduleContainer `json:"queryhorarios"`
+}
+
+type scheduleContainer struct {
+	Container *scheduleDataContainer `json:"data"`
+}
+
+type scheduleDataContainer struct {
+	Groups []courseGroup `json:"grupos_por_asignatura"`
+}
+
+type courseGroup struct {
+	Details *groupDetails `json:"grupo__grupo"`
+	Staff   []groupStaff  `json:"grupo__profesores"`
+}
+
+type groupDetails struct {
+	Signups        int              `json:"grupo__alumnos"`
+	Capacity       int              `json:"grupo__cupo"`
+	HasDescription bool             `json:"grupo__tiene_presentacion"`
+	Code           string           `json:"grupo__clave"`
+	ID             int              `json:"grupo__id"`
+	Name           *groupCourseName `json:"grupo__asignatura"`
+}
+
+type groupCourseName struct {
+	Name string `json:"asignatura__nombre"`
+}
+
+type groupStaff struct {
+	Details      *staffDetails       `json:"profesor__persona"`
+	Availability []staffAvailability `json:"profesor__horarios"`
+}
+
+type staffDetails struct {
+	ID        int    `json:"persona__id"`
+	Name      string `json:"persona__nombre"`
+	FirstName string `json:"persona__apellido_1"`
+	LastName  string `json:"persona__apellido_2"`
+}
+
+type staffAvailability struct {
+	Location    *staffAvailabilityLocation `json:"profesor_horario__lugar"`
+	OnMonday    bool                       `json:"profesor_horario__lu"`
+	OnTuesday   bool                       `json:"profesor_horario__ma"`
+	OnWednesday bool                       `json:"profesor_horario__mi"`
+	OnThursday  bool                       `json:"profesor_horario__ju"`
+	OnFriday    bool                       `json:"profesor_horario__vi"`
+	OnSaturday  bool                       `json:"profesor_horario__sa"`
+	OnSunday    bool                       `json:"profesor_horario__do"`
+	StartTime   string                     `json:"profesor_horario__hora_inicio"`
+	EndTime     string                     `json:"profesor_horario__hora_termino"`
+	Role        *staffRole                 `json:"grupo__cargo"`
+}
+
+type staffRole struct {
+	Name string `json:"cargo__nombre_corto"`
+}
+
+type staffAvailabilityLocation struct {
+	ID   int    `json:"lugar__id"`
+	Name string `json:"lugar__nombre"`
+}
+
 const (
 	baseURL = "https://pagina.fciencias.unam.mx/"
 )
@@ -88,8 +153,21 @@ func Run() error {
 	if err := json.Unmarshal([]byte(planData), planContent); err != nil {
 		return err
 	}
+
+	course := planContent.Container.Container.Container[0].Courses[4].Course
+	courseData, err := foo(fmt.Sprintf("docencia/horarios/20212/%d/%d", plan.ID, course.ID))
+	if err != nil {
+		return err
+	}
+	courseOffer := &courseOfferPayload{}
+	if err := json.Unmarshal([]byte(courseData), courseOffer); err != nil {
+		return err
+	}
+
+	fmt.Println(courseOffer)
 	fmt.Println(planContent)
 	fmt.Println(content)
+
 	return nil
 }
 
